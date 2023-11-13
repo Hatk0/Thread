9d2603c9bd417f7adf2c5867cb3580b5e005b8a6
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-11-13 18:31:39 +0300

    Create two subclasses

--- Thread.playground/Contents.swift
+++ Thread.playground/Contents.swift
@@ -1,3 +1,11 @@
-import UIKit
+import Foundation
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
 
-var greeting = "Hello, playground"
+class GeneratorThread: Thread {
+    
+}
+
+class WorkerThread: Thread {
+    
+}
