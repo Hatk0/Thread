c309d508cba492acc42e7717f859f66f12b59ec9
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-11-14 19:30:49 +0300

    Create a LIFO-based storage facility

--- Thread.playground/Contents.swift
+++ Thread.playground/Contents.swift
@@ -2,6 +2,29 @@ import Foundation
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+class ThreadSafe<T> {
+    private var storage = [T]()
+    private let queue = DispatchQueue(label: "com.my.Thread")
+    
+    func push(_ item: T) {
+        queue.sync {
+            storage.append(item)
+        }
+    }
+    
+    func pop() -> T? {
+        return queue.sync {
+            storage.popLast()
+        }
+    }
+    
+    var isEmpty: Bool {
+        return queue.sync {
+            storage.isEmpty
+        }
+    }
+}
+
 class GeneratorThread: Thread {
     
 }
