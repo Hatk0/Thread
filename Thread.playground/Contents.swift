449b1239efcd9fa88e4daa223d1aa59242194eb2
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-11-14 20:09:05 +0300

    In the WorkerThread make a Chip wait, if they are not there, it pauses in the wait

--- Thread.playground/Contents.swift
+++ Thread.playground/Contents.swift
@@ -43,5 +43,20 @@ class GeneratorThread: Thread {
 }
 
 class WorkerThread: Thread {
+    private let storage: ThreadSafe<Chip>
     
+    init(storage: ThreadSafe<Chip>) {
+        self.storage = storage
+        super.init()
+    }
+    
+    override func main() {
+        while true {
+            if let chip = storage.pop() {
+                chip.sodering()
+            } else {
+                sleep(1)
+            }
+        }
+    }
 }
